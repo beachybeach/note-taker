bbff7308e3c2d1e8ece37ac705e4e2301179fb73
Riley <rsbeach97@gmail.com>
2020-12-13 21:03:23 -0700

    finally deletes note

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,17 @@ const path = require("path");
 const fs = require("fs");
 
 router.get("/notes", (req, res) => {
-  const notes = require("../Develop/db/db.json");
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../Develop/db/db.json"), "utf8")
+  );
   res.json(notes);
   res.status(200).end();
 });
 
 router.post("/notes", (req, res) => {
-  const notes = require("../Develop/db/db.json");
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../Develop/db/db.json"), "utf8")
+  );
   req.body.id = notes.length.toString();
   const note = req.body;
   notes.push(note);
@@ -21,7 +25,9 @@ router.post("/notes", (req, res) => {
 });
 
 router.delete("/notes/:id", (req, res) => {
-  const notes = require("../Develop/db/db.json");
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../Develop/db/db.json"), "utf8")
+  );
   const notesAfterDeletion = notes.filter(({ id }) => {
     return req.params.id !== id;
   });
