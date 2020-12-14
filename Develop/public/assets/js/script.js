2fdbd5569067770897acb18eca0a915489381679
Riley <rsbeach97@gmail.com>
2020-12-13 17:44:07 -0700

    get started opens notes page but css not working

---
+++ Develop/public/assets/js/script.js
@@ -0,0 +1,7 @@
+const $getStartedBtn = document.querySelector("#btn");
+
+const getStartedSubmit = () => {
+  console.log("surprised i made it this far");
+};
+
+$getStartedBtn.addEventListener("click", getStartedSubmit);
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
-
-const PORT = 3001;
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./develop/public/index.html"));
 });
 
-//TODO: Need landing page "get started" button to open the note taker page. (GET request?)
+//links get started button to actual notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+app.use(express.static("public"));
+
 //TODO: Need note taker to be able to accept new notes. (POST request?)
 //TODO: Need to make it so when the write icon is clicked then an empty field appears for new note title and note body.
 //TODO: When a new note is entered then the save icon appears at the top of the page.
